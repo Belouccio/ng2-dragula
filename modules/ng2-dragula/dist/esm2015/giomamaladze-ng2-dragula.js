380b307569ebe470bfb906ca88c81a988009b3ea
Георгий <george@topntop.ru>
2019-04-22 19:21:52 +0300

    change dragula to custom fork with "mixed" direction

---
+++ modules/ng2-dragula/dist/esm2015/giomamaladze-ng2-dragula.js
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { DragulaDirective, DragulaService, DragulaModule, dragula, DrakeFactory, Group, EventTypes, MockDrake, MockDrakeFactory } from './public_api';
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2lvbWFtYWxhZHplLW5nMi1kcmFndWxhLmpzIiwic291cmNlUm9vdCI6Im5nOi8vQGdpb21hbWFsYWR6ZS9uZzItZHJhZ3VsYS8iLCJzb3VyY2VzIjpbImdpb21hbWFsYWR6ZS1uZzItZHJhZ3VsYS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBSUEsdUlBQWMsY0FBYyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBHZW5lcmF0ZWQgYnVuZGxlIGluZGV4LiBEbyBub3QgZWRpdC5cbiAqL1xuXG5leHBvcnQgKiBmcm9tICcuL3B1YmxpY19hcGknO1xuIl19
